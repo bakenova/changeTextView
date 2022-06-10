09856051bae1532a82237aa03ff6ed0575f7bc44
Arailym <arailym.bakenovaa@gmail.com>
2022-06-09 21:13:54 -0700

    add framework

---
+++ changeTextView/changeTextView/TextViewClass.swift
@@ -0,0 +1,14 @@
+//
+//  TextViewClass.swift
+//  changeTextView
+//
+//  Created by Arailym on 09.06.2022.
+//
+
+import Foundation
+
+public struct TextViewClass {
+    public static func getText() -> String {
+        return "Here is another text for the textview"
+    }
+}
---
+++ changeTextView/changeTextView/changeTextView.h
@@ -0,0 +1,18 @@
+//
+//  changeTextView.h
+//  changeTextView
+//
+//  Created by Arailym on 09.06.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for changeTextView.
+FOUNDATION_EXPORT double changeTextViewVersionNumber;
+
+//! Project version string for changeTextView.
+FOUNDATION_EXPORT const unsigned char changeTextViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <changeTextView/PublicHeader.h>
+
+
